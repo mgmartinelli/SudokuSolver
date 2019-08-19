ae99cd76324e803cbcf07a807e54ef7d05e92a7e
Mariela Guardado-Martinelli <mgmartinelli1@gmail.com>
2019-08-18 21:18:09 -0400

    Created XCode Project

---
+++ SudokuSolver/SudokuSolver/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SudokuSolver
+//
+//  Created by Mariela Guardado-Martinelli on 8/18/19.
+//  Copyright © 2019 Mariela Guardado-Martinelli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
